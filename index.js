9d1083edace2242fc758ca8ba1dbeb7a50df58d6
likhithkp <likhithkpdev@gmail.com>
2024-11-01 07:24:23 +0530

    Practice

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-//Arrays
-//Map, filter, forEach, indexOf
+//Fundamentals of Js
+//Arrays and Objects
+//Functions & Returns
+//Async Js coding
+//Map, filter, forEach, indexOf, find
+
 var a = [1, 2, 3]
+
+// a.forEach((num) => {
+//     console.log(num + 'Hello');
+// });
+
+// var b = a.map((num) => {
+//     return num + 13;
+// });
+
+// var b = a.filter((num) => {
+//     return num > 2
+// })
+
+// var b = a.find((num) => {
+//     return num === 2;
+// });
+
+// var b = a.indexOf(20)
+
+console.log(b)
